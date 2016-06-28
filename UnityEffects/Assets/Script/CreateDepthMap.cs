446d3a67823cf8673e453f05a0de504b4f439f79
527984372@qq.com <1234woaiyy>
2016-06-28 19:27:18 +0800

    开发中。。。

--- UnityEffects/Assets/Script/CreateDepthMap.cs
+++ UnityEffects/Assets/Script/CreateDepthMap.cs
@@ -5,18 +5,63 @@ public class CreateDepthMap : MonoBehaviour
 {
     public Material material;
     public Shader depthMapShader;
+    private Camera _mainCamera;
+    private Camera _lightCamera;
 	void Start () 
     {
-        Camera camera = GetComponent<Camera>();
-        camera.depthTextureMode = DepthTextureMode.Depth;
-        camera.clearFlags = CameraClearFlags.SolidColor;
-        camera.backgroundColor = Color.white;
-        camera.SetReplacementShader(depthMapShader, "RenderType");
+        _lightCamera = GetComponent<Camera>();
+        _lightCamera.depthTextureMode = DepthTextureMode.Depth;
+        _lightCamera.clearFlags = CameraClearFlags.SolidColor;
+        _lightCamera.backgroundColor = Color.white;
+        _lightCamera.SetReplacementShader(depthMapShader, "RenderType");
         RenderTexture depthMap = new RenderTexture(Screen.width, Screen.height, 0);
         depthMap.format = RenderTextureFormat.ARGB32;
-        camera.targetTexture = depthMap;
+        _lightCamera.targetTexture = depthMap;
+        //
+        foreach (Camera item in Camera.allCameras)
+        {
+            if (item.CompareTag("MainCamera"))
+            {
+                _mainCamera = item;
+                break;
+            }
+        }
 	}
 
+    void Update()
+    {//根据主相机的视锥来确定灯光相机的设置
+        //1、	求光view矩阵  用world to camera
+        Matrix4x4 viewM = _lightCamera.worldToCameraMatrix;
+        Vector4 v =  viewM * Vector4.one ;
+        //2、	求视锥8顶点  n平面（aspect * y, tan(r/2)* n,n）  f平面（aspect*y, tan(r/2) * f, f）
+        float r = (_mainCamera.fieldOfView / 180f) * Mathf.PI;
+        //n平面
+        Vector4 nLeftUp = new Vector4(-_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, _mainCamera.nearClipPlane, 1);
+        Vector4 nRightUp = new Vector4(_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, _mainCamera.nearClipPlane, 1);
+        Vector4 nLeftDonw = new Vector4(-_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, -Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, _mainCamera.nearClipPlane, 1);
+        Vector4 nRightDonw = new Vector4(_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, -Mathf.Tan(r / 2) * _mainCamera.nearClipPlane, _mainCamera.nearClipPlane, 1);
+
+        //f平面
+        Vector4 fLeftUp = new Vector4(-_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.farClipPlane, Mathf.Tan(r / 2) * _mainCamera.farClipPlane, _mainCamera.farClipPlane, 1);
+        Vector4 fRightUp = new Vector4(_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.farClipPlane, Mathf.Tan(r / 2) * _mainCamera.farClipPlane, _mainCamera.farClipPlane, 1);
+        Vector4 fLeftDonw = new Vector4(-_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.farClipPlane, -Mathf.Tan(r / 2) * _mainCamera.farClipPlane, _mainCamera.farClipPlane, 1);
+        Vector4 fRightDonw = new Vector4(_mainCamera.aspect * Mathf.Tan(r / 2) * _mainCamera.farClipPlane, -Mathf.Tan(r / 2) * _mainCamera.farClipPlane, _mainCamera.farClipPlane, 1);
+
+        //3、	把顶点变换到光view空间
+        Vector4 vnLeftUp = viewM * nLeftUp;
+        Vector4 vnRightUp = viewM * nRightUp;
+        Vector4 vnLeftDonw = viewM * nLeftDonw;
+        Vector4 vnRightDonw = viewM * nRightDonw;
+        //
+        Vector4 vfLeftUp = viewM * fLeftUp;
+        Vector4 vfRightUp = viewM * fRightUp;
+        Vector4 vfLeftDonw = viewM * fLeftDonw;
+        Vector4 vfRightDonw = viewM * fRightDonw;
+
+        //4、	求包围盒 (由于光锥的对称性，这里求最大包围盒就好，不是严格意义的AABB)
+        float maxZ = 
+
+    }
     //void OnRenderImage(RenderTexture source, RenderTexture destination)
     //{
     //    Graphics.Blit(source, destination, material);
